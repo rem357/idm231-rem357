36d1f2e3c68355a0e222f1c017e976ee1d289299
riley mitchell <rem357@drexel.edu>
2025-03-20 21:13:40 -0400

    compress images

--- postload.js
+++ postload.js
@@ -89,7 +89,7 @@ function displayDayInfo(whichOne) {
     case "A Partridge in a Pear Tree":
       zImageObj.src = "https://github.com/rem357/idm231-rem357/blob/main/12%20images/1-1.png?raw=true";
       zDateRangeObj.innerHTML = "Jan 20 - Feb 18";
-      zDescriptionObj.innerHTML = "On the first day of Christmas my true love gave to me a partridge in a pear tree. 	The Partridge is a symbol of stability and protection, making those born under its sign natural caretakers. Just as the lone partridge remains steadfast in its tree, these individuals are grounded, reliable, and self-sufficient. However, their adaptability allows them to navigate change with grace. They are both the pillar of support for loved ones and the free spirit who can thrive in any environment. Their dual nature makes them excellent problem-solvers and mediators in times of conflict.";
+      zDescriptionObj.innerHTML = "On the first day of Christmas my true love gave to me a partridge in a pear tree. 	The Partridge is a symbol of stability and protection, making those born during this time period natural caretakers. Just as the lone partridge remains steadfast in its tree, these individuals are grounded, reliable, and self-sufficient. However, their adaptability allows them to navigate change with grace. They are both the pillar of support for loved ones and the free spirit who can thrive in any environment. Their dual nature makes them excellent problem-solvers and mediators in times of conflict.";
       DisplayAreaObj.classList.remove("hideMe")
       playAudio("day-1");
       break;
@@ -97,7 +97,7 @@ function displayDayInfo(whichOne) {
     case "Two Turtle Doves":
       zImageObj.src = "https://github.com/rem357/idm231-rem357/blob/main/12%20images/2-1.png?raw=true";
       zDateRangeObj.innerHTML = "Feb 19 - Mar 20";
-      zDescriptionObj.innerHTML = "On the second day of Christmas my true love gave to me two turtle doves. 	Two turtle doves represent deep emotional bonds, faithfulness, and devotion. Those born under this sign have a heart full of love and a soul deeply attuned to others’ emotions. They seek meaningful connections in life, prioritizing loyalty and compassion over fleeting pleasures. They are romantics at heart, often seeing the world through a poetic lens. Just as turtle doves mate for life, these individuals value strong, lasting relationships and will go to great lengths to nurture their bonds.";
+      zDescriptionObj.innerHTML = "On the second day of Christmas my true love gave to me two turtle doves. 	Two turtle doves represent deep emotional bonds, faithfulness, and devotion. Those born during this time period have a heart full of love and a soul deeply attuned to others’ emotions. They seek meaningful connections in life, prioritizing loyalty and compassion over fleeting pleasures. They are romantics at heart, often seeing the world through a poetic lens. Just as turtle doves mate for life, these individuals value strong, lasting relationships and will go to great lengths to nurture their bonds.";
       DisplayAreaObj.classList.remove("hideMe");
       playAudio("day-2");
       break;
@@ -105,7 +105,7 @@ function displayDayInfo(whichOne) {
     case "Three French Hens":
       zImageObj.src = "https://github.com/rem357/idm231-rem357/blob/main/12%20images/3-1.png?raw=true";
       zDateRangeObj.innerHTML = "Mar 21 - Apr 19";
-      zDescriptionObj.innerHTML = "On the third day of Christmas my true love gave to me three French hens. French hens were once considered a valuable gift, symbolizing wealth and sustenance. Those born under this sign possess an air of sophistication, resourcefulness, and quiet strength. They are deeply intuitive and know how to transform scarcity into abundance. With an alluring mystery about them, they draw people in with their depth and insight. They are dependable yet selective, ensuring they give their best only to those who truly deserve it.";
+      zDescriptionObj.innerHTML = "On the third day of Christmas my true love gave to me three French hens. French hens were once considered a valuable gift, symbolizing wealth and sustenance. Those born during this time period possess an air of sophistication, resourcefulness, and quiet strength. They are deeply intuitive and know how to transform scarcity into abundance. With an alluring mystery about them, they draw people in with their depth and insight. They are dependable yet selective, ensuring they give their best only to those who truly deserve it.";
       DisplayAreaObj.classList.remove("hideMe");
       playAudio("day-3");
       break;
@@ -113,7 +113,7 @@ function displayDayInfo(whichOne) {
     case "Four Calling Birds":
       zImageObj.src = "https://github.com/rem357/idm231-rem357/blob/main/12%20images/4-1.png?raw=true";
       zDateRangeObj.innerHTML = "Apr 20 - May 20";
-      zDescriptionObj.innerHTML = "On the fourth day of Christmas my true love gave to me four calling birds. Calling birds are known for their beautiful, attention-grabbing songs, just like those born under this sign who captivate others with their charisma and expressiveness. They are natural performers, thriving in environments where their voice can be heard. Their presence is impossible to ignore, and they inspire others with their confidence and passion. Whether through words, music, or leadership, they have a unique ability to draw people in and create a lasting impact.";
+      zDescriptionObj.innerHTML = "On the fourth day of Christmas my true love gave to me four calling birds. Calling birds are known for their beautiful, attention-grabbing songs, just like those born during this time period who captivate others with their charisma and expressiveness. They are natural performers, thriving in environments where their voice can be heard. Their presence is impossible to ignore, and they inspire others with their confidence and passion. Whether through words, music, or leadership, they have a unique ability to draw people in and create a lasting impact.";
       DisplayAreaObj.classList.remove("hideMe");
       DisplayAreaObj.classList.remove("showMe");
       playAudio("day-4");
@@ -122,7 +122,7 @@ function displayDayInfo(whichOne) {
     case "FIVE GOLDEN RINGS!":
       zImageObj.src = "https://github.com/rem357/idm231-rem357/blob/main/12%20images/5-1.png?raw=true";
       zDateRangeObj.innerHTML = "May 21 - Jun 21";
-      zDescriptionObj.innerHTML = "On the fifth day of Christmas my true love gave to me FIVE GOLDEN RINGS! 	Golden rings represent prosperity, prestige, and legacy—qualities deeply ingrained in those born under this sign. They are steadfast in their ambitions, radiating an aura of timeless elegance and strength. Like gold, their loyalty is unwavering, and they place great value on building a life of comfort and stability. They take pride in their achievements and possessions but never let material wealth overshadow their deep appreciation for love and tradition.";
+      zDescriptionObj.innerHTML = "On the fifth day of Christmas my true love gave to me FIVE GOLDEN RINGS! 	Golden rings represent prosperity, prestige, and legacy—qualities deeply ingrained in those born during this time period. They are steadfast in their ambitions, radiating an aura of timeless elegance and strength. Like gold, their loyalty is unwavering, and they place great value on building a life of comfort and stability. They take pride in their achievements and possessions but never let material wealth overshadow their deep appreciation for love and tradition.";
       DisplayAreaObj.classList.remove("hideMe");
       playAudio("day-5");
       break;
@@ -130,7 +130,7 @@ function displayDayInfo(whichOne) {
     case "Six Geese a Laying":
       zImageObj.src = "https://github.com/rem357/idm231-rem357/blob/main/12%20images/6-1.png?raw=true";
       zDateRangeObj.innerHTML = "Jun 22 - Jul 22";
-      zDescriptionObj.innerHTML = "On the sixth day of Christmas my true love gave to me six geese a laying. 	Geese are symbols of abundance, productivity, and nurturing energy. Those born under this sign are generous and always seeking to create something meaningful—whether it’s a family, a career, or an adventure-filled life. They have a restless spirit and are driven by a desire for expansion and exploration. They take pride in fostering growth in those around them, encouraging others to follow their dreams just as they chase their own.";
+      zDescriptionObj.innerHTML = "On the sixth day of Christmas my true love gave to me six geese a laying. 	Geese are symbols of abundance, productivity, and nurturing energy. Those born during this time period are generous and always seeking to create something meaningful—whether it’s a family, a career, or an adventure-filled life. They have a restless spirit and are driven by a desire for expansion and exploration. They take pride in fostering growth in those around them, encouraging others to follow their dreams just as they chase their own.";
       DisplayAreaObj.classList.remove("hideMe");
       playAudio("day-6");
       break;
@@ -138,7 +138,7 @@ function displayDayInfo(whichOne) {
     case "Seven Swans a Swimming":
       zImageObj.src = "https://github.com/rem357/idm231-rem357/blob/main/12%20images/7-1.png?raw=true";
       zDateRangeObj.innerHTML = "Jul 23 - Aug 22";
-      zDescriptionObj.innerHTML = "On the seventh day of Christmas my true love gave to me seven swans a swimming. 	Swans glide effortlessly across the water, symbolizing grace, inner peace, and quiet transformation. Those born under this sign exude an ethereal beauty, but beneath their calm exterior lies a powerful force for change. They are visionaries, always looking to the future and seeking deeper meaning in life. They navigate challenges with poise and have an innate ability to inspire others with their wisdom and unconventional approach to the world.";
+      zDescriptionObj.innerHTML = "On the seventh day of Christmas my true love gave to me seven swans a swimming. 	Swans glide effortlessly across the water, symbolizing grace, inner peace, and quiet transformation. Those born during this time period exude an ethereal beauty, but beneath their calm exterior lies a powerful force for change. They are visionaries, always looking to the future and seeking deeper meaning in life. They navigate challenges with poise and have an innate ability to inspire others with their wisdom and unconventional approach to the world.";
       DisplayAreaObj.classList.remove("hideMe");
       playAudio("day-7");
       break;
@@ -146,7 +146,7 @@ function displayDayInfo(whichOne) {
     case "Eight Maids a Milking":
       zImageObj.src = "https://github.com/rem357/idm231-rem357/blob/main/12%20images/8-1.png?raw=true";
       zDateRangeObj.innerHTML = "Aug 23 - Sep 22";
-      zDescriptionObj.innerHTML = "On the eighth day of Christmas my true love gave to me eight maids a milking. 	Milkmaids represent diligence, humility, and service, making those born under this sign the backbone of any endeavor. They are hardworking, meticulous, and find fulfillment in helping others. Though they often work behind the scenes, their impact is undeniable. They are practical problem-solvers who bring order to chaos and ensure that everything runs smoothly. Their humble nature makes them deeply respected, and they take pride in the simple joys of life.";
+      zDescriptionObj.innerHTML = "On the eighth day of Christmas my true love gave to me eight maids a milking. 	Milkmaids represent diligence, humility, and service, making those born during this time period the backbone of any endeavor. They are hardworking, meticulous, and find fulfillment in helping others. Though they often work behind the scenes, their impact is undeniable. They are practical problem-solvers who bring order to chaos and ensure that everything runs smoothly. Their humble nature makes them deeply respected, and they take pride in the simple joys of life.";
       DisplayAreaObj.classList.remove("hideMe");
       playAudio("day-8");
       break;
@@ -154,7 +154,7 @@ function displayDayInfo(whichOne) {
     case "Nine Ladies Dancing":
       zImageObj.src = "https://github.com/rem357/idm231-rem357/blob/main/12%20images/9-1.png?raw=true";
       zDateRangeObj.innerHTML = "Sep 23 - Oct 23";
-      zDescriptionObj.innerHTML = "On the ninth day of Christmas my true love gave to me nine ladies dancing. 	Dancing ladies symbolize structured creativity, discipline, and celebration. Those born under this sign have an innate ability to balance responsibility with joy, ensuring that even the hardest work is infused with passion. They are determined and ambitious but also know how to appreciate life’s little victories. They bring energy and excitement wherever they go, making them natural leaders in both professional and social settings.";
+      zDescriptionObj.innerHTML = "On the ninth day of Christmas my true love gave to me nine ladies dancing. 	Dancing ladies symbolize structured creativity, discipline, and celebration. Those born during this time period have an innate ability to balance responsibility with joy, ensuring that even the hardest work is infused with passion. They are determined and ambitious but also know how to appreciate life’s little victories. They bring energy and excitement wherever they go, making them natural leaders in both professional and social settings.";
       DisplayAreaObj.classList.remove("hideMe");
       playAudio("day-9");
       break;
@@ -162,7 +162,7 @@ function displayDayInfo(whichOne) {
     case "Ten Lords a Leaping":
       zImageObj.src = "https://github.com/rem357/idm231-rem357/blob/main/12%20images/10-1.png?raw=true";
       zDateRangeObj.innerHTML = "Oct 24 - Nov 21";
-      zDescriptionObj.innerHTML = "On the tenth day of Christmas my true love gave to me ten lords a leaping. Leaping lords embody confidence, strength, and bold movement—qualities that define those born under this sign. They are fearless in their pursuits, taking charge of any situation with an unshakable belief in their abilities. They thrive on adventure, competition, and breaking boundaries. Their leadership qualities make them natural pioneers, always willing to take the first step into the unknown.";
+      zDescriptionObj.innerHTML = "On the tenth day of Christmas my true love gave to me ten lords a leaping. Leaping lords embody confidence, strength, and bold movement—qualities that define those born during this time period. They are fearless in their pursuits, taking charge of any situation with an unshakable belief in their abilities. They thrive on adventure, competition, and breaking boundaries. Their leadership qualities make them natural pioneers, always willing to take the first step into the unknown.";
       DisplayAreaObj.classList.remove("hideMe");
       playAudio("day-10");
       break;
@@ -170,7 +170,7 @@ function displayDayInfo(whichOne) {
     case "Eleven Pipers Piping":
       zImageObj.src = "https://github.com/rem357/idm231-rem357/blob/main/12%20images/11-1.png?raw=true";
       zDateRangeObj.innerHTML = "Nov 22 - Dec 21";
-      zDescriptionObj.innerHTML = "On the eleventh day of Christmas my true love gave to me eleven pipers piping. 	Pipers symbolize harmony, music, and the power of emotional expression. Those born under this sign have a deeply artistic and intuitive nature, using creativity to bring people together. They are emotionally in tune with others, often serving as the mediators and peacemakers in their circles. Whether through music, writing, or simple heartfelt conversations, they have a way of uplifting spirits and creating a sense of unity.";
+      zDescriptionObj.innerHTML = "On the eleventh day of Christmas my true love gave to me eleven pipers piping. 	Pipers symbolize harmony, music, and the power of emotional expression. Those born during this time period have a deeply artistic and intuitive nature, using creativity to bring people together. They are emotionally in tune with others, often serving as the mediators and peacemakers in their circles. Whether through music, writing, or simple heartfelt conversations, they have a way of uplifting spirits and creating a sense of unity.";
       DisplayAreaObj.classList.remove("hideMe");
       playAudio("day-11");
       break;
@@ -178,7 +178,7 @@ function displayDayInfo(whichOne) {
     case "Twelve Drummers Drumming":
       zImageObj.src = "https://github.com/rem357/idm231-rem357/blob/main/12%20images/12-1.png?raw=true";
       zDateRangeObj.innerHTML = "Dec 22 - Jan 19";
-      zDescriptionObj.innerHTML = "On the twelfth day of Christmas my true love gave to me twelve drummers drumming. Drummers represent rhythm, discipline, and balance—qualities that those born under this sign embody in every aspect of life. They have a strong sense of justice and fairness, ensuring that everything they do aligns with their core values. They are persistent and determined, keeping their momentum even in the face of challenges. Their ability to bring people together and maintain harmony makes them natural leaders and respected voices in any community.";
+      zDescriptionObj.innerHTML = "On the twelfth day of Christmas my true love gave to me twelve drummers drumming. Drummers represent rhythm, discipline, and balance—qualities. Those born during this time period embody in every aspect of life. They have a strong sense of justice and fairness, ensuring that everything they do aligns with their core values. They are persistent and determined, keeping their momentum even in the face of challenges. Their ability to bring people together and maintain harmony makes them natural leaders and respected voices in any community.";
       DisplayAreaObj.classList.remove("hideMe");
       playAudio("day-12");
       break;
